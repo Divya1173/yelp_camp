b2c0a8113b417824038ba80e5cdfe8e3870779f5
Divya Mohan <thakurdivya.mohan@gmail.com>
2019-05-19 08:43:35 +0000

    Seperated Middleware Code from Route File

---
+++ workspace/middleware/index.js
@@ -0,0 +1,46 @@
+var middlewareObj={};
+var Comment =require("../models/comment");
+var Campground =require("../models/campground");
+middlewareObj.checkCommentOwnership = function(req,res,next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id,function(err,foundComment){
+            if(err){
+                res.redirect("back");
+            }else{
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        });
+    }else{
+        res.redirect("back");
+    }
+};
+
+middlewareObj.checkOwnership = function(req,res,next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id,function(err,foundCampground){
+            if(err){
+                res.redirect("back");
+            }else{
+                if(foundCampground.author.id.equals(req.user.id)){
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        });
+    }else{
+        res.redirect("back");
+    }
+};
+middlewareObj.isLogged=function(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports= middlewareObj
--- workspace/routes/campgrounds.js
+++ workspace/routes/campgrounds.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router  = express.Router();
-var Campground =require("../models/campground");
+var Campground = require("../models/campground");
+var middleware = require("../middleware");
 
 router.get("/campgrounds",function(req, res){
     Campground.find({},function(err,allCampgrounds){
@@ -15,7 +16,7 @@ router.get("/campgrounds",function(req, res){
     });
 });
 
-router.post("/campgrounds",isLogged,function(req, res) {
+router.post("/campgrounds",middleware.isLogged,function(req, res) {
     var name=req.body.name;
     var image=req.body.image;
     var description=req.body.description;
@@ -35,7 +36,7 @@ router.post("/campgrounds",isLogged,function(req, res) {
         }
     });
 });
-router.get("/campgrounds/new",isLogged,function(req, res) {
+router.get("/campgrounds/new",middleware.isLogged,function(req, res) {
     res.render("campgrounds/new");
 });
 router.get("/campgrounds/:id",function(req, res) {
@@ -50,7 +51,7 @@ router.get("/campgrounds/:id",function(req, res) {
     });
 });
 // Edit campground route 
-router.get("/campgrounds/:id/edit",checkOwnership,function(req, res) {
+router.get("/campgrounds/:id/edit",middleware.checkOwnership,function(req, res) {
     Campground.findById(req.params.id,function(err,foundCampground){
         if(err){
             res.redirect("/campgrounds");
@@ -61,7 +62,7 @@ router.get("/campgrounds/:id/edit",checkOwnership,function(req, res) {
 
 });
 // Update campground route
-router.put("/campgrounds/:id",checkOwnership,function(req,res){
+router.put("/campgrounds/:id",middleware.checkOwnership,function(req,res){
     Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
         if(err){
             res.redirect("/campgrounds");
@@ -72,7 +73,7 @@ router.put("/campgrounds/:id",checkOwnership,function(req,res){
 });
 
 // Destroy Campground Route
-router.delete("/campgrounds/:id",checkOwnership,function(req,res){
+router.delete("/campgrounds/:id",middleware.checkOwnership,function(req,res){
     Campground.findByIdAndRemove(req.params.id,function(err){
         if(err){
             res.redirect("/campgrounds");
@@ -80,29 +81,7 @@ router.delete("/campgrounds/:id",checkOwnership,function(req,res){
         res.redirect("/campgrounds");
     });
 });
-function checkOwnership(req,res,next){
-    if(req.isAuthenticated()){
-        Campground.findById(req.params.id,function(err,foundCampground){
-            if(err){
-                res.redirect("back");
-            }else{
-                if(foundCampground.author.id.equals(req.user.id)){
-                    next();
-                }else{
-                    res.redirect("back");
-                }
-            }
-        });
-    }else{
-        res.redirect("back");
-    }
-};
 
-function isLogged(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+
 
 module.exports=router;
