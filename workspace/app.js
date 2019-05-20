9e4467c8b2d735280ba7578b309262869ae14753
Divya Mohan <thakurdivya.mohan@gmail.com>
2019-05-20 11:48:46 +0000

    Completed Landing Page

--- workspace/app.js
+++ workspace/app.js
@@ -42,6 +42,7 @@ app.use(function(req,res,next){
   res.locals.success     = req.flash("success");
   next();
 });
+app.use(express.static(__dirname + '/public'));
 app.use(methodOverride("_method"));
 app.use(commentRoutes);
 app.use(indexRoutes);
